6026412a557737b866ed878d4092df371a169890
unknown <souravsharma7194@gmail.com>
2025-12-10 23:49:30 +0530

    new app with nextjs

---
+++ app/about/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const page:React.FC  = () => {
+  return (
+    <div className=''>page
+    <div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</div>
+  )
+}
+
+export default page
---
+++ app/login/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const page : React.FC = () => {
+  return (
+
+
+<form > 
+
+<h2>Login</h2>
+<div>
+<label htmlFor="email">Email</label>
+<input type="text"
+id= "email"
+/>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</form>
+
+
+
+
+
+  )
+}
+
+export default page
